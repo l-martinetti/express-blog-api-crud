420ac104cbcc1e93e7e21b4f3c1dd0616872f444
l-martinetti <luca.martinetti@outlook.it>
2025-01-09 11:56:43 +0100

    feat: add error handlers
    - Base excercise complete

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+const posts = require('./data/posts');
+const routerPosts = require('./routers/posts');
+const errorHandler = require('./middlewares/errorsHandler');
+const notFound = require('./middlewares/notFound');
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-const posts = require('./data/posts');
-const routerPosts = require('./routers/posts');
-
+// body-parser
 app.use(express.json());
 
 app.use(express.static('public'));
 
+// router
 app.use('/posts', routerPosts);
 
+// main route
 app.get('/', (req, res) => {
     res.send('Server Blog post')
 });
+
+// handlers errori
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => {
     console.log('funziono')
